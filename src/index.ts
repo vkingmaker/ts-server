719677560e8aa9a9382643ac639f55e39637b7f0
Monday Victor A <vkingmaker1@gmail.com>
2019-08-12 01:05:08 +0100

    feature: user authentication
    This commit adds a user authentioncation server built
    with typescript and utilizing the power of decorators
    and seesion based authentication
    
    Delivers [#authentication]

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express, { Response, Request } from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['lajdhdfkjhasf'] }));
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
